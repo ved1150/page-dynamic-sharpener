7689970a8e4a42a539638144751753d1dd361405
ved1150 <109255020+ved1150@users.noreply.github.com>
2022-07-25 17:20:16 +0530

    Update callback.js

--- callback.js
+++ callback.js
@@ -1,13 +1,14 @@
 let posts =[
-    {tital : "post one" , body:" this is the post one" ,createdAt : timestamp  },
-    {tital : "post two" , body:" this is the post two"}
+    {tital : "post one" , body:" this is the post one" ,createdAt : new Date().getTime()  },
+    {tital : "post two" , body:" this is the post two",createdAt : new Date().getTime() }
 ];
-
+let myInterval;
 function getPost(){
-    setTimeout(() => {
+    clearInterval(myInterval)
+    setInterval(() => {
         let outPut ="";
-        posts.forEach((post ,index  ) => {
-            outPut += `<li>${post.tital}</li>`        
+        posts.forEach((post) => {
+            outPut += `<li>${post.tital} lastUpdate ${ (new Date().getTime() - post.createdAt)/1000} second ago </li>`        
         });
         document.body.innerHTML= outPut;
     }, 1000);
@@ -24,8 +25,9 @@ function creatPost(post,callback){
 
  function creatPost(post,callback){
     setTimeout(() => {
-        posts.push(post)
+        posts.push({...post, createdAt:new Date().getTime()})
         callback()
-    }, 5000);
+    }, 8000);
 }
  creatPost({tital : "post four" , body:" this is the post four"},getPost)
+
